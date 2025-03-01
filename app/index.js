ca550812a434b2f774e1777e621f643d4de305fd
Steve Sajeev <stevesajeev123@gmail.com>
2025-03-01 15:14:07 -0500

    Finish home page

--- app/index.js
+++ app/index.js
@@ -1,4 +1,8 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, Text, Button, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+
+import logo from "@assets/logo.png";
+import StyledButton from "@components/StyledButton";
 
 /*
   Route: / (Entry Point)
@@ -6,15 +10,67 @@ import { View, Text, StyleSheet } from "react-native";
   Displays login and create account buttons
 */
 export default function Index() {
-  return <View style={styles.container}>
-    <Text>Hello, world!</Text>
-  </View>
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.logo} source={logo} />
+        <Text style={[styles.title, styles.study]}>Study</Text>
+        <Text style={[styles.title, styles.gator]}>Gator</Text>
+      </View>
+      <View style={styles.buttons}>
+        <StyledButton
+          text="Login"
+          backgroundColor="#FA4616"
+          pressedColor="#cf360e"
+          onClick={() => router.navigate("/login")}
+        />
+        <StyledButton
+          text="Create Account"
+          backgroundColor="#FA4616"
+          pressedColor="#cf360e"
+          onClick={() => router.navigate("/register")}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#292D5D',
+    alignItems: 'center',
+    paddingVertical: 60
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  logo: {
+    width: 100,
+    height: 100
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    textShadowColor: 'black',
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 10
+  },
+  study: {
+    color: '#FA4616'
+  },
+  gator: {
+    color: '#0021A5'
+  },
+  buttons: {
+    flex: 1,
+    gap: 40,
+    width: '80%',
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center'
   }
---
+++ components/StyledButton.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Pressable, Text, StyleSheet } from "react-native";
+
+/* 
+  Styled Button Component - Displays button with text
+
+  props: {
+    text: Text to display on button,
+    backgroundColor: Background color of button,
+    pressedColor: Background color of button when pressed,
+    onClick: Method to be called on button press
+  }
+*/
+export default function StyledButton(props) {
+  const [pressed, setPressed] = useState(false);
+
+  return (
+    <Pressable 
+      style={[styles.button, { backgroundColor: pressed ? props.pressedColor : props.backgroundColor }]}
+      onPressIn={() => setPressed(true)}
+      onPressOut={() => setPressed(false)}
+      onPress={props.onClick}
+    >
+      <Text style={styles.text}>{props.text}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 60,
+    borderRadius: 4
+  },
+  text: {
+    fontSize: 20,
+    color: 'white'
+  }
+});
