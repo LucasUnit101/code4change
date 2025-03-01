5a9879a990567130e5281db14d0ff451b35b4d01
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-03-01 16:24:04 -0500

    friend + button updated

--- frontend/app/(tabs)/friends.js
+++ frontend/app/(tabs)/friends.js
@@ -1,7 +1,62 @@
+import StyledButton from "@components/StyledButton";
+import { View, Text, StyleSheet } from "react-native";
+
 /*
   Route: /friends
 */
 
 export default function Friends() {
-  return null;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Friends</Text>
+        <View style={styles.buttonContainer}>
+          <StyledButton 
+            text="Add Friend"
+            backgroundColor="white"
+            borderColor='black'
+            pressedColor='#D3D3D3'
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+
+  header: {
+    flexDirection: "row", 
+    alignItems: "center",  
+    justifyContent: "space-between",
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  buttonContainer: {
+    marginLeft: 10, 
+    width: 120,
+    height: 60,
+  },
+
+  button: {
+    borderRadius: 20,
+  },
+
+  title: {
+    color: "black",
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "left",
+    padding: 20, 
+  },
+});
