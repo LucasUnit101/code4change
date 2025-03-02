68bf153249b7398711d05ff76a125985fc9a12d2
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-03-01 23:17:41 -0500

    fetch time

--- frontend/app/(tabs)/home.js
+++ frontend/app/(tabs)/home.js
@@ -3,9 +3,9 @@ import { StyleSheet, View, Text, Pressable, ScrollView } from "react-native";
 import { useSession } from "@context/ctx";
 import Constants from "expo-constants";
 
-import Ionicons from '@expo/vector-icons/Ionicons';
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-import Clock from '@components/Clock';
+import Clock from "@components/Clock";
 
 /*
   Route: /home
@@ -18,7 +18,7 @@ export default function Home() {
 
   const timer = useRef();
   const pausedRef = useRef(paused);
-  
+
   useEffect(() => {
     pausedRef.current = paused;
   }, [paused]);
@@ -28,15 +28,15 @@ export default function Home() {
     setPressed(true);
     timer.current = setInterval(() => {
       if (pausedRef.current) return;
-      setTime(prevTime => prevTime + 1);
+      setTime((prevTime) => prevTime + 1);
     }, 1000);
   }, [pressed, paused]);
 
   const pauseTimer = () => {
-    setPaused(prevPaused => !prevPaused);
-  }
+    setPaused((prevPaused) => !prevPaused);
+  };
 
-  const stopTimer = () => {
+  const stopTimer = async () => {
     clearInterval(timer.current);
     timer.current = undefined;
     setPaused(false);
@@ -45,82 +45,102 @@ export default function Home() {
 
     // Make a request to the backend with time
     const URI = Constants.expoConfig.hostUri.split(":").shift();
-    fetch(`http://${URI}:${process.env.EXPO_PUBLIC_PORT}/profiles/${session}`);
-    fetch(`http://${URI}:${process.env.EXPO_PUBLIC_PORT}/:profileID/${session}`);
-  }
+    const profile = await fetch(
+      `http://${URI}:${process.env.EXPO_PUBLIC_PORT}/profiles/${session}`
+    ).then((res) => res.json());
+
+    await fetch(
+      `http://${URI}:${process.env.EXPO_PUBLIC_PORT}/profiles/${profile.id}/time`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          time,
+        }),
+      }
+    );
+  };
 
   return (
     <ScrollView>
       <View style={styles.title}>
-      <Text style= {styles.font}>Start Your Study Jouney</Text>
+        <Text style={styles.font}>Start Your Study Jouney</Text>
       </View>
-    <View style={styles.container}>
-      <Pressable
-        style={[styles.button, { backgroundColor: pressed ? 'green': 'red' }]}
-        onPress={handlePress}
-      >
-        {!pressed ?
-          <Text style={styles.text}>Start</Text> :
-          (
+      <View style={styles.container}>
+        <Pressable
+          style={[
+            styles.button,
+            { backgroundColor: pressed ? "green" : "red" },
+          ]}
+          onPress={handlePress}
+        >
+          {!pressed ? (
+            <Text style={styles.text}>Start</Text>
+          ) : (
             <View style={styles.controlsContainer}>
               <Clock time={time} />
               <View style={styles.controls}>
                 <Pressable onPress={pauseTimer}>
-                  <Ionicons name={paused ? "play" : "pause"} size={60} color="white" />
+                  <Ionicons
+                    name={paused ? "play" : "pause"}
+                    size={60}
+                    color="white"
+                  />
                 </Pressable>
                 <Pressable onPress={stopTimer}>
                   <Ionicons name="stop" size={60} color="white" />
                 </Pressable>
               </View>
             </View>
-          )
-        }
-      </Pressable>
-    </View>
-        </ScrollView>
-  )
+          )}
+        </Pressable>
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
     paddingTop: 80,
   },
-  font:{
+  font: {
     paddingTop: 30,
     fontSize: 40,
-    fontWeight: 'bold',
-    color: 'black',
-    fontStyle: 'italic',
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    color: "black",
+    fontStyle: "italic",
+    fontWeight: "bold",
   },
 
   title: {
-    borderBottomColor: 'black',
+    borderBottomColor: "black",
     borderBottomWidth: 1,
     paddingHorizontal: 20,
   },
   button: {
     aspectRatio: 1,
-    borderRadius: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+    borderRadius: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 30,
-    color: 'white'
+    color: "white",
   },
   controlsContainer: {
-    display: 'flex',
+    display: "flex",
     gap: 20,
-    alignItems: 'center'
+    alignItems: "center",
   },
   controls: {
-    display: 'flex',
-    flexDirection: 'row',
-    gap: 10
-  }
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+  },
 });
